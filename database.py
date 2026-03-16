141d193777e79ae33dfab8ef1aefcbe2ddea583f
Mpho Louis Chauke <mpholouischauke@outlook.com>
2026-03-16 12:17:39 +0200

    add: async operations

---
+++ database.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db():
+   async with AsyncSessionLocal() as session:
+        yield session
---
+++ schemas.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    image_file: str | None
+    image_path: str
+
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
+
+
+class PostBase(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    content: str = Field(min_length=1)
+
+
+class PostCreate(PostBase):
+    user_id: int  # TEMPORARY
+
+
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    content: str = Field(default=None, min_length=1)
+
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    user_id: int
+    date_posted: datetime
+    author: UserResponse
+
+
+
+
+    user_update = user_data.model_dump(exclude_unset=True)
+
+    for field, value in user_update.items():
+        setattr(user, field, value)
